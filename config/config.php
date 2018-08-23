28c4e914ed1b18402d2e5e3b9ecbc6c435025821
nirajspitze9 <niraj.visana@gmail.com>
2018-08-23 20:11:50 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -14,10 +14,10 @@
 
 	define('NO_OF_FOLLOWERS', 10);
 	
-	defined('CONSUMER_KEY') || define('CONSUMER_KEY', 'VYjyS0kyIAQtO9wMmH9Xcc73k'); //Add your twitter consumer key
-	defined('CONSUMER_SECRET') || define('CONSUMER_SECRET', '5fEwBA8OO3mjSkKhbeTfu6W4tXY81gGaO8EBUi5FWi7xGEGX92'); //Add twitter consumer secret key
-	defined('TWITTER_ACCESS_TOKEN') || define('TWITTER_ACCESS_TOKEN', '531047460-00K62ZnUkdNfxota5fx9bZb28mIw3aaGFJR9cgnU'); //Add twitter access token 
-	defined('TWITTER_ACCESS_TOKEN_SECRET') || define('TWITTER_ACCESS_TOKEN_SECRET', '5fEwBA8OO3mjSkKhbeTfu6W4tXY81gGaO8EBUi5FWi7xGEGX92'); //Add twitter access token secret
+	defined('CONSUMER_KEY') || define('CONSUMER_KEY', 'VYjyS0kyIAQtOxxxxxxxx'); //Add your twitter consumer key
+	defined('CONSUMER_SECRET') || define('CONSUMER_SECRET', '5fEwBA8OO3mjSkKXxxxxxxxxxxxxxxxxxxxxxxxxxx'); //Add twitter consumer secret key
+	defined('TWITTER_ACCESS_TOKEN') || define('TWITTER_ACCESS_TOKEN', '5310474xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'); //Add twitter access token 
+	defined('TWITTER_ACCESS_TOKEN_SECRET') || define('TWITTER_ACCESS_TOKEN_SECRET', '5fEwXxxxxxxxxxxxxxxxxxxxxxxxxxxxxx2'); //Add twitter access token secret
 	defined('OAUTH_CALLBACK') || define('OAUTH_CALLBACK', 'http://visana.xyz/twiproj/callback.php'); //Add twitter call back page url of your site
 	
 	defined('ABSOLUTE_CSV_DOWNLOAD_PATH') || define('ABSOLUTE_CSV_DOWNLOAD_PATH', ABSOLUTE_PATH . '/download/csv/');
